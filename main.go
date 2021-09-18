d2a8a0678cf124179798c331ed56a566a5fa6b27
Agus Susilo <smartgdi@gmail.com>
2021-09-18 22:51:52 +0700

    Tambah template

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"github.com/flosch/pongo2"
 	"github.com/gin-gonic/gin"
+	"github.com/stnc/pongo2gin"
 	"github.com/susilolab/gotodo/services"
 )
 
 func main() {
+	gin.SetMode(gin.DebugMode)
 	r := gin.Default()
+	r.Use(gin.Recovery())
+	r.HTMLRender = pongo2gin.TemplatePath("views")
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.HTML(200, "hello.html", pongo2.Context{
+			"title": "Hello pongo",
+		})
+	})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
