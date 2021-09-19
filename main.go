d79fc475abc9be7c4acc350d3f933243f414a37a
Agus Susilo <smartgdi@gmail.com>
2021-09-19 07:48:12 +0700

    update main.go

--- main.go
+++ main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+    "os"
+    "log"
+
 	"github.com/flosch/pongo2"
 	"github.com/gin-gonic/gin"
 	"github.com/stnc/pongo2gin"
+    "github.com/joho/godotenv"
+	"github.com/susilolab/gotodo/models"
 	"github.com/susilolab/gotodo/services"
 )
 
 func main() {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalln(err)
+    }
+    dbUrl := os.Getenv("DATABASE_URL")
+    models.SetupDB(dbUrl)
+
 	gin.SetMode(gin.DebugMode)
 	r := gin.Default()
 	r.Use(gin.Recovery())
 	r.HTMLRender = pongo2gin.TemplatePath("views")
-
+    r.Static("/public", "./public")
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+		c.HTML(200, "index.html", pongo2.Context{
+			"title": "Todo App",
 		})
 	})
 
